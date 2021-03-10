d3764f582b12f6e3af95a49850805535ae3fa5c3
Danillo Aragão <danillo.aragao@rarolabs.com.br>
2021-03-09 23:31:24 -0300

    Seeds adicionados

--- Itau.Teste.API/Itau.Teste.Infra.Data/Context/AppDbContext.cs
+++ Itau.Teste.API/Itau.Teste.Infra.Data/Context/AppDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Itau.Teste.Domain.Entities;
+using Itau.Teste.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Itau.Teste.Infra.Data.Context
 {
@@ -39,6 +41,112 @@ namespace Itau.Teste.Infra.Data.Context
                     .HasColumnName("CONCILIADO")
                     .HasColumnType("tinyint(1)");
             }).Entity<LancamentoFinanceiro>().Property(l => l.Id).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<LancamentoFinanceiro>().HasData(
+                new LancamentoFinanceiro
+                {
+                    Id = 1,
+                    DataHoraLancamento = new DateTime(2021, 3, 1, 12, 4, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Credito,
+                    Valor = 3000
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 2,
+                    DataHoraLancamento = new DateTime(2021, 3, 1, 13, 3, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Debito,
+                    Valor = 100
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 3,
+                    DataHoraLancamento = new DateTime(2021, 3, 1, 14, 2, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Debito,
+                    Valor = 55
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 4,
+                    DataHoraLancamento = new DateTime(2021, 3, 2, 14, 3, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Credito,
+                    Valor = 72
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 5,
+                    DataHoraLancamento = new DateTime(2021, 3, 2, 14, 47, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Debito,
+                    Valor = 153
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 6,
+                    DataHoraLancamento = new DateTime(2021, 3, 3, 15, 43, 0),
+                    Conciliado = true,
+                    Tipo = TipoLancamentoFinanceiro.Debito,
+                    Valor = 342
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 7,
+                    DataHoraLancamento = new DateTime(2021, 3, 3, 15, 32, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Debito,
+                    Valor = 75
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 8,
+                    DataHoraLancamento = new DateTime(2021, 3, 3, 16, 57, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Debito,
+                    Valor = 145
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 9,
+                    DataHoraLancamento = new DateTime(2021, 3, 3, 17, 34, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Credito,
+                    Valor = 234
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 10,
+                    DataHoraLancamento = new DateTime(2021, 3, 4, 17, 41, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Credito,
+                    Valor = 876
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 11,
+                    DataHoraLancamento = new DateTime(2021, 4, 4, 18, 35, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Credito,
+                    Valor = 234
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 12,
+                    DataHoraLancamento = new DateTime(2021, 4, 4, 11, 24, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Credito,
+                    Valor = 56
+                },
+                new LancamentoFinanceiro
+                {
+                    Id = 13,
+                    DataHoraLancamento = new DateTime(2021, 4, 4, 11, 21, 0),
+                    Conciliado = false,
+                    Tipo = TipoLancamentoFinanceiro.Credito,
+                    Valor = 213
+                });
         }
     }
 }
---
+++ Itau.Teste.API/Itau.Teste.Infra.Data/Migrations/20210310021608_SeedAjustesFinanceiro.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Itau.Teste.Infra.Data.Migrations
+{
+    public partial class SeedAjustesFinanceiro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                columns: new[] { "ID", "CONCILIADO", "DATA_HORA_LANCAMENTO", "TIPO", "VALOR" },
+                values: new object[,]
+                {
+                    { 1, false, new DateTime(2021, 3, 1, 12, 4, 0, 0, DateTimeKind.Unspecified), 2, 3000m },
+                    { 2, false, new DateTime(2021, 3, 1, 13, 3, 0, 0, DateTimeKind.Unspecified), 1, 100m },
+                    { 3, false, new DateTime(2021, 3, 1, 14, 2, 0, 0, DateTimeKind.Unspecified), 1, 55m },
+                    { 4, false, new DateTime(2021, 3, 2, 14, 3, 0, 0, DateTimeKind.Unspecified), 2, 72m },
+                    { 5, false, new DateTime(2021, 3, 2, 14, 47, 0, 0, DateTimeKind.Unspecified), 1, 153m },
+                    { 6, true, new DateTime(2021, 3, 3, 15, 43, 0, 0, DateTimeKind.Unspecified), 1, 342m },
+                    { 7, false, new DateTime(2021, 3, 3, 15, 32, 0, 0, DateTimeKind.Unspecified), 1, 75m },
+                    { 8, false, new DateTime(2021, 3, 3, 16, 57, 0, 0, DateTimeKind.Unspecified), 1, 145m },
+                    { 9, false, new DateTime(2021, 3, 3, 17, 34, 0, 0, DateTimeKind.Unspecified), 2, 234m },
+                    { 10, false, new DateTime(2021, 3, 4, 17, 41, 0, 0, DateTimeKind.Unspecified), 2, 876m },
+                    { 11, false, new DateTime(2021, 4, 4, 18, 35, 0, 0, DateTimeKind.Unspecified), 2, 234m },
+                    { 12, false, new DateTime(2021, 4, 4, 11, 24, 0, 0, DateTimeKind.Unspecified), 2, 56m },
+                    { 13, false, new DateTime(2021, 4, 4, 11, 21, 0, 0, DateTimeKind.Unspecified), 2, 213m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "teste_itau.LANCAMENTOS_FINANCEIROS",
+                keyColumn: "ID",
+                keyValue: 13);
+        }
+    }
+}
